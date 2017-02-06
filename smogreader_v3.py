21149e13de1c07d90200ea10780bb0130b8ee0a2
pawel-manu <manowiecki@gmail.com>
2017-02-06 19:56:40 +0100

    Corrections as suggested by @nooga
    Corrected JSON Payload preparation for Requests

--- smogreader_v3.py
+++ smogreader_v3.py
@@ -24,30 +24,31 @@ while True:
 	pm25 = (output[8]*256 + output[7])/10.0
 	pm10 = (output[12]*256 + output[11])/10.0
 
-        # read output of BME280
-        temperature = round(sensor.read_temperature(),2)
-        pressure_pascals = round(sensor.read_pressure(),2)
-        pressure = round(pressure_pascals / 100,2)
-        humidity = round(sensor.read_humidity(),2)
+    # read output of BME280
+    temperature = round(sensor.read_temperature(),2)
+    pressure_pascals = round(sensor.read_pressure(),2)
+    pressure = round(pressure_pascals / 100,2)
+    humidity = round(sensor.read_humidity(),2)
 
-        # get timestamps
-        timestamp = datetime.datetime.now()
-        n = datetime.datetime.now()
-        unixtimestamp = time.mktime(n.timetuple())
+    # get timestamps
+    timestamp = datetime.datetime.now()
+    n = datetime.datetime.now()
+    unixtimestamp = time.mktime(n.timetuple())
 
-        #build content for file, dweet.io, opensmog.api
+    #build content for file, dweet.io, opensmog.api
 	filecontent = str(timestamp) + ';' + str(pm25) + ';' + str(pm10) + ';' + str(temperature) + ';' + str(pressure) + ';' + str(humidity) + '\n'
-        dweetcontent = 'timestamp=' + str(int(unixtimestamp)) + '&pm25=' + str(pm25) + '&pm10=' + str(pm10)+ '&temp=' + str(temperature)+ '&press=' + str(pressure) + '&hum=' + str(humidity)
-        opensmogjson = '[{ "timestamp": ' + str(int(unixtimestamp)) + ',"readings": {"PM2_5": ' + str(pm25) +',"PM10": ' + str(pm10) +',"TEMP": ' + str(temperature) +',"HUM": ' + str(humidity) +',"PRES": ' + str(pressure) +'}}]'
+    dweetcontent = 'timestamp=' + str(int(unixtimestamp)) + '&pm25=' + str(pm25) + '&pm10=' + str(pm10)+ '&temp=' + str(temperature)+ '&press=' + str(pressure) + '&hum=' + str(humidity)
+    opensmogjson = [{ "timestamp" : int(unixtimestamp) , "readings" : {"pm2_5": pm25 , "pm10" : pm10 , "temp": temperature , "hum" : humidity , "press":  pressure }}]
 	
 	f = open('smogreader_sdl_bme.txt','a')
 	f.write(filecontent)
 	f.close() 
-	r = requests.get('http://dweet.io/dweet/for/{your-dweet-id}?key={your-dweet-key}&'+dweetcontent)
-	s = requests.post('http://httpbin.org/post', json=opensmogjson)
+	r = requests.get('http://dweet.io/dweet/for/{SUID}?key={KEY}&'+dweetcontent)
+	s = requests.post('http://{SMOGAPI_ENDPOINT}/sensors/{SUID}/readings', json=opensmogjson)
 	
 # OUTPUTS for debug
 #        print(filecontent)
 #        print(dweetcontent)
-#        print(opensmogjson)                                                         
-                                                                                         
+#        print(opensmogjson)
+#		 print(s)
+#		 print(s.text)
