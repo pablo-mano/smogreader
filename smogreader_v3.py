81c176860eb845b4c3c898d2bc25420f51d6ff27
pawel-manu <manowiecki@gmail.com>
2017-02-07 07:33:27 +0100

    Error handling
    Finally errors handled with grace :)

--- smogreader_v3.py
+++ smogreader_v3.py
@@ -17,38 +17,55 @@ print('connected to: ' + ser.portstr + ' at ' + str(datetime.datetime.now()))
 #read and send data to file, dweet.io and opensmog API
 while True:
 
-        # read output of SDL607
- 	output =  struct.unpack('BBBBBBBBBBBBBBBBBBB',ser.read(19))
+        try:
 
-        #parse output to particle matter measures
-	pm25 = (output[8]*256 + output[7])/10.0
-	pm10 = (output[12]*256 + output[11])/10.0
+                # read output of SDL607
+                output =  struct.unpack('BBBBBBBBBBBBBBBBBBB',ser.read(19))
 
-    # read output of BME280
-    temperature = round(sensor.read_temperature(),2)
-    pressure_pascals = round(sensor.read_pressure(),2)
-    pressure = round(pressure_pascals / 100,2)
-    humidity = round(sensor.read_humidity(),2)
+                #parse output to particle matter measures
+                pm25 = (output[8]*256 + output[7])/10.0
+                pm10 = (output[12]*256 + output[11])/10.0
 
-    # get timestamps
-    timestamp = datetime.datetime.now()
-    n = datetime.datetime.now()
-    unixtimestamp = time.mktime(n.timetuple())
+                # read output of BME280
+                temperature = round(sensor.read_temperature(),2)
+                pressure_pascals = round(sensor.read_pressure(),2)
+                pressure = round(pressure_pascals / 100,2)
+                humidity = round(sensor.read_humidity(),2)
 
-    #build content for file, dweet.io, opensmog.api
-	filecontent = str(timestamp) + ';' + str(pm25) + ';' + str(pm10) + ';' + str(temperature) + ';' + str(pressure) + ';' + str(humidity) + '\n'
-    dweetcontent = 'timestamp=' + str(int(unixtimestamp)) + '&pm25=' + str(pm25) + '&pm10=' + str(pm10)+ '&temp=' + str(temperature)+ '&press=' + str(pressure) + '&hum=' + str(humidity)
-    opensmogjson = [{ "timestamp" : int(unixtimestamp) , "readings" : {"pm2_5": pm25 , "pm10" : pm10 , "temp": temperature , "hum" : humidity , "press":  pressure }}]
+                # get timestamps
+                timestamp = datetime.datetime.now()
+                n = datetime.datetime.now()
+                unixtimestamp = time.mktime(n.timetuple())
+
+                #build content for file, dweet.io, opensmog.api
+                filecontent = str(timestamp) + ';' + str(pm25) + ';' + str(pm10) + ';' + str(temperature) + ';' + str(pressure) + ';' + str(humidity) + '\n'
+                dweetcontent = 'timestamp=' + str(int(unixtimestamp)) + '&pm25=' + str(pm25) + '&pm10=' + str(pm10)+ '&temp=' + str(temperature)+ '&press=' + str(pressure) + '&hum=' + str(humidity)
+                opensmogjson = [{ "timestamp" : int(unixtimestamp) , "readings" : {"pm2_5": pm25 , "pm10" : pm10 , "temp": temperature , "hum" : humidity , "press":  pressure }}]
+                
+                f = open('smogreader_sdl_bme.txt','a')
+                f.write(filecontent)
+                f.close() 
+                r = requests.get('http://dweet.io/dweet/for/{SUID}?key={KEY}&'+dweetcontent)
+                s = requests.post('http://{SMOGAPI}/sensors/{SUID}/readings', json=opensmogjson)
 	
-	f = open('smogreader_sdl_bme.txt','a')
-	f.write(filecontent)
-	f.close() 
-	r = requests.get('http://dweet.io/dweet/for/{SUID}?key={KEY}&'+dweetcontent)
-	s = requests.post('http://{SMOGAPI_ENDPOINT}/sensors/{SUID}/readings', json=opensmogjson)
-	
-# OUTPUTS for debug
-#        print(filecontent)
-#        print(dweetcontent)
-#        print(opensmogjson)
-#		 print(s)
-#		 print(s.text)
+        # OUTPUTS for debug
+        # posting content to echo API
+        #	x = requests.post('https://postman-echo.com/post',json=opensmogjson)
+        #	print(x.text)
+
+        # preview of content for API's
+        #        print(filecontent)
+        #        print(dweetcontent)
+                print(opensmogjson)
+                print(s)
+                print(s.text)
+
+        # exception handling with gracefull register & continue
+        except Exception as e:
+                timestamp = datetime.datetime.now()
+                print "!!! " + str(timestamp) + " Error occured"
+                print type(e)
+                print e.args
+                print e
+                pass
+                
